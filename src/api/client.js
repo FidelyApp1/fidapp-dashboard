6e438b25403220bb4b1184732924eced36a8000a
CnssStockManag <stockmanagcnss@gmail.com>
2026-06-15 03:10:21 +0100

    fix: vercel routing + API URL production

--- src/api/client.js
+++ src/api/client.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  baseURL: 'https://fidapp-backend-production.up.railway.app/api',
   headers: { 'Content-Type': 'application/json' }
 })
 
